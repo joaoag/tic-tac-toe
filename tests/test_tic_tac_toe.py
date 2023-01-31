ea0d242628426c74aba21fbdf83d4b025fe80f9c
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2023-01-31 21:18:59 +0000

    test: failing test for returning the board

--- tests/test_tic_tac_toe.py
+++ tests/test_tic_tac_toe.py
@@ -18,5 +18,21 @@ def test_game_stores_user_input(monkeypatch):
 def test_game_only_allows_nine_moves():
     game = Game()
     game.moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    with pytest.raises(BoardFullException):
+    with pytest.raises(
+        BoardFullException, match="Sorry, the board is full so the game is over"
+    ):
         game.add_move(10)
+
+
+def test_game_shows_player_the_board():
+    game = Game()
+
+    expected = """      
+         |     |     
+    -----|-----|-----
+         |     |     
+    -----|-----|-----
+         |     |     
+    """
+    actual = game.get_board()
+    assert expected == actual
