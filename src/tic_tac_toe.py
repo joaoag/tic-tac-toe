0dc006d2924a4debd15060cd3e2e79d22b65c9e9
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2023-01-31 21:03:47 +0000

    feat: game stops playing more moves than the board allows

--- src/tic_tac_toe.py
+++ src/tic_tac_toe.py
@@ -1,6 +1,10 @@
 class BoardFullException(Exception):
     pass
 
+
+MAXIMUM_MOVES = 9
+
+
 class Game:
     def __init__(self):
         self.moves = []
@@ -14,10 +18,16 @@ class Game:
         return next_move
 
     def add_move(self, position):
-        self.moves.append(position)
+        if self.count_moves() < MAXIMUM_MOVES:
+            self.moves.append(position)
+        else:
+            raise BoardFullException("Sorry, the board is full so the game is over")
 
     def get_moves(self):
         return self.moves
+
+    def count_moves(self):
+        return len(self.get_moves())
 
 
 if __name__ == "__main__":
