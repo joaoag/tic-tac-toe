80cd178604b48eea18fdb18c7a396c87653b6979
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2023-02-01 14:00:08 +0000

    test: add test to document validation for player character input

--- tests/test_game.py
+++ tests/test_game.py
@@ -29,10 +29,25 @@ def test_game_gets_and_saves_players_characters(monkeypatch):
     monkeypatch.setattr("sys.stdin", first_player_choice)
     game = Game(MagicMock)
     game.request_first_character()
-    expected = {
+    expected_order = {
         1: "O",
         2: "X"
     }
-    actual = game._get_play_order()
-    assert expected == actual
+    actual_order = game._get_play_order()
+    assert expected_order == actual_order
+
+
+def test_game_only_allows_player_to_choose_x_or_o(monkeypatch):
+    first_player_choice = StringIO("A\n")
+    monkeypatch.setattr("sys.stdin", first_player_choice)
+    game = Game(MagicMock)
+
+    expected_state = dict()
+    expected_message = "Sorry, that's not a valid character, you must pick X or O"
+
+    actual_message = game.request_first_character()
+    actual_state = game._get_play_order()
+
+    assert expected_state == actual_state
+    assert expected_message == actual_message
 
