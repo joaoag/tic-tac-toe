82a624ac9b681fdf00e8af23a51f42f6f768560a
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2023-02-05 12:52:39 +0000

    test: failing integration test for Play class (preparing to refactor Game class so it is not responsible for running itself)

---
+++ tests/test_play.py
@@ -0,0 +1,21 @@
+from io import StringIO
+
+from src.game import Game
+from src.board import Board
+
+
+def test_play_allows_game_to_be_won(monkeypatch):
+    x_o_moves_with_x_win = ['1', '9', '2', '8']
+    first_player_chooses_x = "X\n"
+    player_input_for_x_win = '\n'.join(x_o_moves_with_x_win)
+    players_input = StringIO(f"{first_player_chooses_x}{player_input_for_x_win}")
+    monkeypatch.setattr("sys.stdin", players_input)
+
+    board = Board()
+    game = Game(board)
+    play = Play(game)
+    play.play()
+
+    expected_winner = "X"
+    actual_winner = play.game._get_winner()
+    assert expected_winner == actual_winner
